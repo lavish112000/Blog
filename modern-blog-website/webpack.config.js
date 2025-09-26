69cabfe121fbc940f362e1a6044c377a82f11f31
Lavish <lalitchoudhary112000@gmail.com>
2025-09-26 19:24:52 +0530

    Update webpack config with build optimizations

--- modern-blog-website/webpack.config.js
+++ modern-blog-website/webpack.config.js
@@ -1,36 +1,153 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
+    mode: isProduction ? 'production' : 'development',
     entry: {
-        main: './src/js/main.js',
-        blog: './src/js/blog.js',
-        components: './src/js/components.js'
+        main: './src/js/main-enterprise.js',
+        blog: './src/js/blog-enterprise.js',
+        components: './src/js/components-enterprise.js',
+        analytics: './src/js/analytics.js',
+        pwa: './src/js/pwa.js'
     },
     output: {
-        filename: '[name].bundle.js',
+        filename: isProduction ? '[name].[contenthash].js' : '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true
+        clean: true,
+        publicPath: '/',
+        assetModuleFilename: 'assets/[name].[contenthash][ext]'
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
             },
             {
-                test: /\.(png|jpg|gif|svg|woff|woff2|eot|ttf)$/,
-                type: 'asset/resource'
+                test: /\.css$/,
+                use: [
+                    isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    ['autoprefixer'],
+                                    ...(isProduction ? [['cssnano']] : [])
+                                ]
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name].[contenthash][ext]'
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name].[contenthash][ext]'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
             }
         ]
     },
-    devtool: 'source-map',
-    devServer: {
-        static: './dist',
-        hot: true
-    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            chunks: ['main', 'components', 'analytics', 'pwa'],
+            minify: isProduction ? {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true
+            } : false
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/blog.html',
+            filename: 'pages/blog.html',
+            chunks: ['blog', 'components', 'analytics'],
+            minify: isProduction
+        }),
+        ...(isProduction ? [
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash].css'
+            })
+        ] : []),
+        ...(process.env.ANALYZE ? [new BundleAnalyzerPlugin()] : [])
+    ],
     optimization: {
         splitChunks: {
-            chunks: 'all'
+            chunks: 'all',
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                    priority: 20
+                },
+                common: {
+                    name: 'common',
+                    minChunks: 2,
+                    chunks: 'all',
+                    priority: 10
+                }
+            }
+        },
+        runtimeChunk: 'single',
+        moduleIds: 'deterministic',
+        usedExports: true,
+        sideEffects: false
+    },
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'src'),
+            publicPath: '/'
+        },
+        compress: true,
+        port: 3000,
+        hot: true,
+        open: true,
+        historyApiFallback: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
         }
+    },
+    resolve: {
+        extensions: ['.js', '.json'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@utils': path.resolve(__dirname, 'src/js/utils'),
+            '@styles': path.resolve(__dirname, 'src/css')
+        }
+    },
+    performance: {
+        hints: isProduction ? 'warning' : false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
     }
 };
