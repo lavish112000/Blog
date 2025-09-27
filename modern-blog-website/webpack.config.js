6a2c6dd3633678cea6d553013f2384daa39574ac
Lavish <lalitchoudhary112000@gmail.com>
2025-09-27 18:27:28 +0530

    Update webpack configuration for modern build process

--- modern-blog-website/webpack.config.js
+++ modern-blog-website/webpack.config.js
@@ -1,153 +1,36 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-
-const isProduction = process.env.NODE_ENV === 'production';
+const path = require("path");
 
 module.exports = {
-    mode: isProduction ? 'production' : 'development',
-    entry: {
-        main: './src/js/main-enterprise.js',
-        blog: './src/js/blog-enterprise.js',
-        components: './src/js/components-enterprise.js',
-        analytics: './src/js/analytics.js',
-        pwa: './src/js/pwa.js'
-    },
-    output: {
-        filename: isProduction ? '[name].[contenthash].js' : '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        publicPath: '/',
-        assetModuleFilename: 'assets/[name].[contenthash][ext]'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-proposal-class-properties']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    ['autoprefixer'],
-                                    ...(isProduction ? [['cssnano']] : [])
-                                ]
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name].[contenthash][ext]'
-                }
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'fonts/[name].[contenthash][ext]'
-                }
-            },
-            {
-                test: /\.html$/,
-                use: ['html-loader']
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            chunks: ['main', 'components', 'analytics', 'pwa'],
-            minify: isProduction ? {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeAttributeQuotes: true
-            } : false
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/blog.html',
-            filename: 'pages/blog.html',
-            chunks: ['blog', 'components', 'analytics'],
-            minify: isProduction
-        }),
-        ...(isProduction ? [
-            new MiniCssExtractPlugin({
-                filename: 'css/[name].[contenthash].css'
-            })
-        ] : []),
-        ...(process.env.ANALYZE ? [new BundleAnalyzerPlugin()] : [])
+  entry: {
+    main: "./src/js/main.js",
+    blog: "./src/js/blog.js",
+    components: "./src/js/components.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|woff|woff2|eot|ttf)$/,
+        type: "asset/resource",
+      },
     ],
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                    priority: 20
-                },
-                common: {
-                    name: 'common',
-                    minChunks: 2,
-                    chunks: 'all',
-                    priority: 10
-                }
-            }
-        },
-        runtimeChunk: 'single',
-        moduleIds: 'deterministic',
-        usedExports: true,
-        sideEffects: false
+  },
+  devtool: "source-map",
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
     },
-    devtool: isProduction ? 'source-map' : 'eval-source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'src'),
-            publicPath: '/'
-        },
-        compress: true,
-        port: 3000,
-        hot: true,
-        open: true,
-        historyApiFallback: true,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
-        }
-    },
-    resolve: {
-        extensions: ['.js', '.json'],
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@components': path.resolve(__dirname, 'src/components'),
-            '@utils': path.resolve(__dirname, 'src/js/utils'),
-            '@styles': path.resolve(__dirname, 'src/css')
-        }
-    },
-    performance: {
-        hints: isProduction ? 'warning' : false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000
-    }
+  },
 };
