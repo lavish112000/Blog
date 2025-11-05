a55039fafeb390547bd5ce34f084ef28016ba701
Lavish <lalitchoudhary112000@gmail.com>
2025-11-05 20:10:19 +0530

    Add Netlify configuration and update webpack for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
     main: "./src/js/main.js",
     blog: "./src/js/blog.js",
     components: "./src/js/components.js",
+    "contact-page": "./src/js/contact-page.js",
   },
   output: {
     filename: "[name].bundle.js",
@@ -23,6 +26,58 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      chunks: ["main", "blog", "components"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/contact.html",
+      filename: "pages/contact.html",
+      chunks: ["contact-page"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/about.html",
+      filename: "pages/about.html",
+      chunks: [],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/blog.html",
+      filename: "pages/blog.html",
+      chunks: ["blog"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/business.html",
+      filename: "pages/business.html",
+      chunks: [],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/design.html",
+      filename: "pages/design.html",
+      chunks: [],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/lifestyle.html",
+      filename: "pages/lifestyle.html",
+      chunks: [],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/technology.html",
+      filename: "pages/technology.html",
+      chunks: [],
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/css", to: "css" },
+        { from: "src/assets", to: "assets" },
+        { from: "src/components", to: "components" },
+        { from: "src/data", to: "data" },
+        { from: "src/manifest.json", to: "manifest.json" },
+        { from: "src/sw.js", to: "sw.js" },
+      ],
+    }),
+  ],
   devtool: "source-map",
   devServer: {
     static: "./dist",
